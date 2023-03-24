1f375ef6142b0e69b4771a5efa0e2671595ba181
Mark Ubiadas <mubiadas@gmail.com>
2023-03-24 16:34:18 -0700

    cart page
    logic for coupon and quantity for cart total price

--- script.js
+++ script.js
@@ -23,13 +23,18 @@ function addToCart(elem) {
     }
 }
 
+
+
 function freeShipping(elem) {
+    var total = 0
+    
+    
     if(document.querySelector(".free-shipping").value == "FREESHIPPING"){
         document.querySelector(".coupon-error").style.color = "green";
         document.querySelector(".coupon-error").innerText = "Coupon Applied";
         document.querySelector(".free-shipping").value = "";
         document.querySelector(".shipping-fee").innerText = "FREE"
-
+        document.querySelector(".cart-total-total").innerText = document.querySelector(".cart-total-subtotal").innerText
         
     } else {
         document.querySelector(".coupon-error").style.color = "red";
@@ -37,3 +42,43 @@ function freeShipping(elem) {
         document.querySelector(".free-shipping").value = "";
     }
 }
+
+// function productSubtotal(elem){
+//     var total = 0;
+//     var shipping = (document.querySelector(".cart-shipping-fee"))
+//     if(document.querySelector(".cart-product-quantity").value !== 1) {
+//         total = document.querySelector(".cart-product-price").innerText * document.querySelector(".cart-product-quantity").value;
+//         document.querySelector(".cart-product-subtotal").innerText = total;
+//         document.querySelector(".cart-total-subtotal").innerText = total;
+
+//         if(shipping.innerText !== "FREE"){
+//             document.querySelector(".cart-total-total").innerText = total + Number(shipping.innerText);
+//             console.log(total)
+//         } else if (shipping.innerText == "FREE") {
+//             document.querySelector(".cart-total-total").innerText = document.querySelector(".cart-total-subtotal").innerText
+//         }
+        
+//     }
+// }
+
+function productSubtotal(elem){
+    var total = 0;
+    var shipping = (document.querySelector(".cart-shipping-fee"))
+    if(document.querySelector(".cart-product-quantity").value !== 1) {
+        total = document.querySelector(".cart-product-price").innerText * document.querySelector(".cart-product-quantity").value;
+        document.querySelector(".cart-product-subtotal").innerText = total;
+        document.querySelector(".cart-total-subtotal").innerText = total;
+
+        if(shipping.innerText !== "FREE"){
+            document.querySelector(".cart-total-total").innerText = total + Number(shipping.innerText);
+        } else if (shipping.innerText == "FREE") {
+            document.querySelector(".cart-total-total").innerText = document.querySelector(".cart-total-subtotal").innerText
+        }
+        
+    }
+    // update cart-total-total element
+    document.querySelector(".cart-total-total").innerText = Number(document.querySelector(".cart-total-subtotal").innerText) + Number(shipping.innerText);
+}
+
+document.querySelector(".cart-product-quantity").addEventListener("change", productSubtotal);
+
